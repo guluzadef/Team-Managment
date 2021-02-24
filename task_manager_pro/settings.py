e0d4339304a4288620624312663f53e4b10db232
Fuad Guluzade <48252040+guluzadef@users.noreply.github.com>
2021-02-24 19:06:26 +0400

    Update settings.py

--- task_manager_pro/settings.py
+++ task_manager_pro/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
+if not DEBUG:
     STATICFILES_DIRS = [
         BASE_DIR / "static",
     ]
